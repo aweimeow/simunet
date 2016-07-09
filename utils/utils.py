4d1266bc02a3b9ad388a7beafae407bb28c6d632
aweimeow <ccps960718456@gmail.com>
2016-07-09 08:58:35 -0400

    fix utils - Cmd

--- utils/utils.py
+++ utils/utils.py
@@ -2,15 +2,19 @@
 from subprocess import Popen, PIPE
 
 
-class cmd:
-    def __init__(self, cmd):
+class Cmd:
+    def __init__(self, mode, cmd):
         self.cmd = cmd
+        if mode == 'safe':
+            self.safeexec()
+        elif mode == 'shell':
+            self.shellexec()
 
     def safeexec(self):
-        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        p = Popen(self.cmd.split(), stdout=PIPE, stderr=PIPE)
         self.stdout, self.stderr = p.communicate()
 
     def shellexec(self):
-        p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+        p = Popen(self.cmd, stdout=PIPE, stderr=PIPE, shell=True)
         self.stdout, self.stderr = p.communicate()
 
