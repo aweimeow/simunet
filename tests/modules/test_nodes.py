50140fbe92e87add9a70cdfe7f4f8dc937b47864
aweimeow <ccps960718456@gmail.com>
2016-07-12 23:53:19 -0400

    change test host name for avoid conflict

--- tests/modules/test_nodes.py
+++ tests/modules/test_nodes.py
@@ -9,22 +9,29 @@ from utils.utils import Cmd
 
 @pytest.fixture(scope="module")
 def host():
-    return Host('h1', '10.0.0.1', mac='00:AA:BB:CC:DD:EE')
+    return Host('host_test')
 
 @pytest.fixture(scope="module")
 def switch():
-    return Switch('sw1')
+    return Switch('switch_test')
+
+@pytest.yield_fixture()
+def run_test():
+    Cmd('safe', 'docker stop host_test')
+    Cmd('safe', 'docker rm host_test')
+    Cmd('safe', 'ovs-vsctl del-br switch_test')
+    yield
 
 @pytest.mark.usefixtures('host')
 class TestHostClass:
     def test_host_create(self, host):
-        p = Cmd('shell', 'docker ps -a | grep h1 | cut -d " " -f 1')
+        p = Cmd('shell', 'docker ps -a | grep host_test | cut -d " " -f 1')
         assert host.container_id[:12] == p.stdout.strip()
 
     def test_host_start(self, host):
         host.start()
 
-        p = Cmd('shell', 'docker ps -a | grep h1')
+        p = Cmd('shell', 'docker ps -a | grep host_test')
         assert re.match('.*Up Less than a second', p.stdout)
 
     def test_host_get_pid(self, host):
@@ -34,26 +41,26 @@ class TestHostClass:
     def test_host_stop(self, host):
         host.stop()
 
-        p = Cmd('shell', 'docker ps -a | grep h1')
+        p = Cmd('shell', 'docker ps -a | grep host_test')
         assert re.match('.*Exited \(\d+\) Less than a second ago', p.stdout)
 
     def test_host_destroy(self, host):
         assert host.status == 'stop' or host.status == 'create'
         host.destroy()
 
-        p = Cmd('shell', 'docker ps -a | grep h1')
+        p = Cmd('shell', 'docker ps -a | grep host_test')
         assert p.stdout.strip() == ''
 
 @pytest.mark.usefixtures('switch')
 class TestSwitchClass:
     def test_switch_create(self, switch):
         p = Cmd('safe', 'ovs-vsctl list-br')
-        assert 'sw1' in p.stdout
+        assert 'switch_test' in p.stdout
 
     def test_switch_destroy(self, switch):
-        p = Cmd('safe', 'ovs-vsctl del-br sw1')
+        p = Cmd('safe', 'ovs-vsctl del-br switch_test')
         assert p.stderr == ''
         
         p = Cmd('safe', 'ovs-vsctl list-br')
-        assert 'sw1' not in p.stdout
+        assert 'switch_test' not in p.stdout
         
